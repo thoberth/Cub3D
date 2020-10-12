a09bb5464302bf3b3c2e70065460340212026fa4
Thomas BERTHET <thoberth@e1r10p16.42.fr>
2020-10-12 17:01:29 +0200

    WIP

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: thoberth <thoberth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/18 10:07:53 by thoberth          #+#    #+#             */
-/*   Updated: 2020/10/12 15:45:58 by thoberth         ###   ########.fr       */
+/*   Updated: 2020/10/12 16:59:01 by thoberth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,28 @@ int		deal_key(int key, t_list_ptr *ptr)
 {
 	return (0);
 }
-  
+
+void	display_map(t_list_ptr *ptr, t_list_map *map)
+{
+	int i = 0;
+	// CHAR 0 1 2 3
+	// CLR	B G R A
+	while (i < map->reso[0] * 4 * map->reso[1])
+	{
+		ptr->data_addr[i] = 0x66;
+		i += (map->reso[0] / map->t_map_x) * 4;
+	}
+	i = 0;
+	while (i < map->reso[0] * 4 * map->reso[1])
+	{
+		i += (map->reso[0] * 4) * (map->reso[1] / map->t_map_y);
+		for (int t = 0 ; t < map->reso[0] * 4 && i < map->reso[0] * 4 * map->reso[1] ; t++)
+			ptr->data_addr[i++] = 0x66;
+	}
+
+	
+}
+
 int main ()
 {
 	t_list_ptr	ptr;
@@ -38,6 +59,7 @@ int main ()
 
 	ptr.img_ptr = mlx_new_image(ptr.mlx_ptr, map.reso[0], map.reso[1]); //stocke le depart de l'image (en haut a gauche)
 	ptr.data_addr = mlx_get_data_addr(ptr.img_ptr, &bpp, &size_line, &endian); //utilise une str pour parcourir les octets de l'img
+	display_map(&ptr, &map);
 	mlx_put_image_to_window(ptr.mlx_ptr, ptr.win_ptr, ptr.img_ptr, 0, 0);
 	mlx_loop(ptr.mlx_ptr);
 	return (0);
