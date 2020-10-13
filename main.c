054ef5e703dd6ae129c95bd63e64b46503353bdf
Thomas BERTHET <thoberth@e1r4p20.42.fr>
2020-10-13 16:32:39 +0200

    WIP

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: thoberth <thoberth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/18 10:07:53 by thoberth          #+#    #+#             */
-/*   Updated: 2020/10/12 16:59:01 by thoberth         ###   ########.fr       */
+/*   Updated: 2020/10/13 15:29:19 by thoberth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,45 @@ int		deal_key(int key, t_list_ptr *ptr)
 	return (0);
 }
 
-void	display_map(t_list_ptr *ptr, t_list_map *map)
+//aff les lignes verticalles de la map
+void 	display_map2(t_list_ptr *ptr, t_list_map *map)
 {
-	int i = 0;
-	// CHAR 0 1 2 3
-	// CLR	B G R A
-	while (i < map->reso[0] * 4 * map->reso[1])
-	{
-		ptr->data_addr[i] = 0x66;
-		i += (map->reso[0] / map->t_map_x) * 4;
-	}
+	int i;
+	int t;
+
 	i = 0;
 	while (i < map->reso[0] * 4 * map->reso[1])
 	{
+		t = 0;
 		i += (map->reso[0] * 4) * (map->reso[1] / map->t_map_y);
-		for (int t = 0 ; t < map->reso[0] * 4 && i < map->reso[0] * 4 * map->reso[1] ; t++)
-			ptr->data_addr[i++] = 0x66;
+		while (t < map->reso[0] * 4 && i < map->reso[0] * 4 * map->reso[1])
+		{
+			ptr->data_addr[++i] = (char)128;
+			t++;
+		}
 	}
+}
 
-	
+//aff les lignes horizontalles de la map
+void	display_map(t_list_ptr *ptr, t_list_map *map)
+{
+	int i;
+	int	t;
+	// CHAR 0 1 2 3
+	// CLR	B G R A
+	i = 0;
+	while (i < map->reso[0] * 4 * map->reso[1])
+	{
+		t = 0;
+		i += (map->reso[0] / map->t_map_x) * 4;
+		while (t < map->reso[0] * 4 && i < map->reso[0] * 4 * map->reso[1])
+		{
+			i += (map->reso[0] / map->t_map_x) * 4;
+			ptr->data_addr[i] = (char)255;
+			t++;
+		}
+	}
+	display_map2(ptr, map);
 }
 
 int main ()
@@ -47,6 +67,12 @@ int main ()
 	ft_init_ptr(&ptr);
 	if (ft_init_map(&map, "fstmap.cub") == -1)
 		return(-1);
+	ft_putnbr(map.t_map_x);
+	ft_putchar('\n');
+	ft_putnbr(map.t_map_y);
+	ft_putchar('\n');
+	ft_putnbr((map.reso[0] / map.t_map_x) * 4);
+	ft_putchar('\n');
 	ptr.mlx_ptr = mlx_init();
 	ptr.win_ptr = mlx_new_window(ptr.mlx_ptr, map.reso[0], map.reso[1], "Cub3D");
 	int bpp;
