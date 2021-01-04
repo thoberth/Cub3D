34178d6a3881a4e246c9ef6d7efca17ef4220adb
Berthet Thomas <thoberth@student.42.fr>
2021-01-04 14:02:57 +0100

    WIP - Linux

--- Cub3d_Linux/lib/ft_raycasting.c
+++ Cub3d_Linux/lib/ft_raycasting.c
@@ -95,12 +95,14 @@ void	ft_raycasting(t_list_map *map)
 	int		bpp;
 	int		endian;
 
-	if (map->data.img_ptr != NULL)
-		mlx_destroy_image(map->data.mlx_ptr, map->data.img_ptr);
-	map->data.img_ptr = mlx_new_image(map->data.mlx_ptr, map->map.reso[0],
-		map->map.reso[1]);
-	map->data.data_addr = mlx_get_data_addr(map->data.img_ptr, &bpp,
-		&map->map.size_line, &endian);
+	mlx_clear_window(map->data.mlx_ptr, map->data.win_ptr);
+	if (map->data.img_ptr == NULL)
+	{
+		map->data.img_ptr = mlx_new_image(map->data.mlx_ptr, map->map.reso[0],
+			map->map.reso[1]);
+		map->data.data_addr = mlx_get_data_addr(map->data.img_ptr, &bpp,
+			&map->map.size_line, &endian);
+	}
 	ft_find_dist(map);
 	ft_display(map);
 	if (map->ray.nbr_sprite > 0)
